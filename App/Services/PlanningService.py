1fdf7b31aff84d5b42fcc28d629bddbd16b978b6
Estebanan03000 <estebanidarragaospina29@gmail.com>
2026-05-14 15:01:00 -0500

    Arreglo del metodo _work en PlanningService.py

--- App/Services/PlanningService.py
+++ App/Services/PlanningService.py
@@ -7,20 +7,23 @@ class PlanningService:
         self.selected_job = job
         
     def applyJobs(self, budget, actualBudget, hours):
-        if actualBudget < 0 or budget < 0:
-            raise Exception("Budget cannot be negative")
+        if actualBudget < 0 or budget < 0 or hours <= 0:
+            raise Exception("Budget cannot be negative or hours cannot be zero or negative")
         
         if actualBudget < (budget * 0.35):
             if self.selected_job is not None:
                 raise Exception("You cannot apply a job when the actual budget is over 35% of the total budget")
-.venv\Scripts\activate
+
+            if hours > self.selected_job.get_max_hours():
                 raise Exception("You cannot apply a job when the hours exceed the maximum hours for that job")
+
             self._work(actualBudget, hours)
     
+
     def _work(self, actualBudget, hours):
         works = self._actualAirports.getJobs()
         if self.selected_job in works:
-            workedHours = works[self.selected_job].get_hourly_rate() * hours
+            index_of_selected_job = works.index(self.selected_job)
+            workedHours = works[index_of_selected_job].get_hourly_rate() * hours
             actualBudget += workedHours
             return actualBudget
-            
