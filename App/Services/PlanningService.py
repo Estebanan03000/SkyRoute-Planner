cf6922f32a446dcde63d8f4fbc92b9675bbab022
CMAG-98 <cesarmateoaguilargarcia1@gmail.com>
2026-05-21 14:52:25 -0500

    avances en la planeacion basica de itinerario

--- App/Services/PlanningService.py
+++ App/Services/PlanningService.py
@@ -1,26 +1,27 @@
+from Models.Graph import Graph
+
 class PlanningService:
+
     def __init__(self, actualAirports):
         self._actualAirports = actualAirports
         self.selected_job = None
 
-    def setSelectedJob(self, job):
-        self.selected_job = job
+    def DjstrakMoney(self, origin, destiny, budget):
+        if origin not in self._actualAirports or destiny not in self._actualAirports:
+            return None
+        else:
+            distances, previous, path = Graph.dijkstra(self._actualAirports, origin, destiny)
+            if distances[destiny] > budget:
+                return None
+            else:
+                return path
         
-    def applyJobs(self, budget, actualBudget, hours):
-        if actualBudget < 0 or budget < 0:
-            raise Exception("Budget cannot be negative")
-        
-        if actualBudget < (budget * 0.35):
-            if self.selected_job is not None:
-                raise Exception("You cannot apply a job when the actual budget is over 35% of the total budget")
-.venv\Scripts\activate
-                raise Exception("You cannot apply a job when the hours exceed the maximum hours for that job")
-            self._work(actualBudget, hours)
-    
-    def _work(self, actualBudget, hours):
-        works = self._actualAirports.getJobs()
-        if self.selected_job in works:
-            workedHours = works[self.selected_job].get_hourly_rate() * hours
-            actualBudget += workedHours
-            return actualBudget
-            
+    def DjstrakTime(self, origin, destiny, max_time):
+        if origin not in self._actualAirports or destiny not in self._actualAirports:
+            return None
+        else:
+            distances, previous, path = Graph.dijkstra_time(self._actualAirports, origin, destiny)
+            if distances[destiny] > max_time:
+                return None
+            else:
+                return path
